525600514c4661c84743a449e46168bfbfdc7c79
Reece Harding <reeceharding@gmail.com>
2025-02-07 15:13:34 -0600

    Initial Commit

---
+++ ProductivityTalk#2/ProductivityTalk_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTalk_2App.swift
+//  ProductivityTalk#2
+//
+//  Created by Reece Harding on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityTalk_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductivityTalk#2Tests/ProductivityTalk_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTalk_2Tests.swift
+//  ProductivityTalk#2Tests
+//
+//  Created by Reece Harding on 2/7/25.
+//
+
+import Testing
+@testable import ProductivityTalk_2
+
+struct ProductivityTalk_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
